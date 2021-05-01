5cab8db5df082552aa357cf8cd7096d00f5844f6
OZimmermann <82403411+OZimmermann@users.noreply.github.com>
2021-05-01 11:41:14 +0200

    Aktualisiere pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-list2: List[number] = []
 def Herzschlag():
     basic.show_icon(IconNames.HEART)
     basic.pause(100)
@@ -13,9 +12,7 @@ else:
     basic.show_number(0)
 for index in range(4):
     basic.show_number(0)
-for Index in range(5):
-    basic.show_number(0)
-for Wert in list2:
+for index2 in range(5):
     basic.show_number(0)
 while input.button_is_pressed(Button.A):
     basic.show_number(0)
@@ -23,3 +20,5 @@ while input.button_is_pressed(Button.A):
     basic.show_number(0)
     if input.button_is_pressed(Button.B):
         break
+for index3 in range(4):
+    Herzschlag()
--- main.ts
+++ main.ts
@@ -1,29 +1,22 @@
-let list2 : number[] = []
-function Herzschlag() {
+function Herzschlag () {
     basic.showIcon(IconNames.Heart)
     basic.pause(100)
     basic.showIcon(IconNames.SmallHeart)
     basic.pause(100)
 }
-
 Herzschlag()
 if (input.buttonIsPressed(Button.A)) {
     basic.showNumber(0)
 }
-
 if (input.pinIsPressed(TouchPin.P0)) {
     basic.showNumber(0)
 } else {
     basic.showNumber(0)
 }
-
 for (let index = 0; index < 4; index++) {
     basic.showNumber(0)
 }
-for (let Index = 0; Index < 5; Index++) {
-    basic.showNumber(0)
-}
-for (let Wert of list2) {
+for (let index = 0; index < 5; index++) {
     basic.showNumber(0)
 }
 while (input.buttonIsPressed(Button.A)) {
@@ -32,7 +25,9 @@ while (input.buttonIsPressed(Button.A)) {
 while (input.buttonIsPressed(Button.A)) {
     basic.showNumber(0)
     if (input.buttonIsPressed(Button.B)) {
-        break
+        break;
     }
-    
+}
+for (let index = 0; index < 4; index++) {
+    Herzschlag()
 }
