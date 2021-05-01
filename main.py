0679d79f473d3f4a5d4296bc5781d3eb5cef761f
OZimmermann <82403411+OZimmermann@users.noreply.github.com>
2021-05-01 10:24:40 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.py
@@ -0,0 +1,10 @@
+if input.button_is_pressed(Button.A):
+    basic.show_number(0)
+if input.pin_is_pressed(TouchPin.P0):
+    basic.show_number(0)
+else:
+    basic.show_number(0)
+for index in range(4):
+    basic.show_number(0)
+for Index in range(5):
+    basic.show_number(0)
---
+++ main.ts
@@ -0,0 +1,14 @@
+if (input.buttonIsPressed(Button.A)) {
+    basic.showNumber(0)
+}
+if (input.pinIsPressed(TouchPin.P0)) {
+    basic.showNumber(0)
+} else {
+    basic.showNumber(0)
+}
+for (let index = 0; index < 4; index++) {
+    basic.showNumber(0)
+}
+for (let Index = 0; Index <= 4; Index++) {
+    basic.showNumber(0)
+}
